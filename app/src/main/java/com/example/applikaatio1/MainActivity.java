5516c760836c874370968a4174a1987724ef3e47
Veikko Moilanen <veikkosakari01@gmail.com>
2021-03-08 17:31:29 +0200

    juuelikkas

--- app/src/main/java/com/example/applikaatio1/MainActivity.java
+++ app/src/main/java/com/example/applikaatio1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.applikaatio1;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    String syote;
     TextView text;
-    TextView text2;
+    EditText tekstiSyote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView);
+        tekstiSyote = (EditText) findViewById(R.id.tekstiSyote);
         text.setText("Tekstijuttu");
     }
 
     public void testFunction (View v) {
         System.out.println("Hello World!");
-        text.setText("Hello World!");
+        syote = tekstiSyote.getText().toString();
+        text.setText(syote);
     }
 }
